2c253ad6e2b85e2f15684d11fa66d856c9ffbd4f
iseejunaid <junaidnadeem266@gmail.com>
2023-08-15 15:50:34 +0500

    updated changes

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
-import { Feather } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons'; 
+import { Feather,Ionicons,FontAwesome } from '@expo/vector-icons'; 
 
 
 export default function App() {
@@ -15,16 +14,27 @@ export default function App() {
     { label: 'Pennsylvania', value: 'PA', latitude: 41.2033, longitude: -77.1945 },
     { label: 'Ohio', value: 'OH', latitude: 40.4173, longitude: -82.9071 },
     { label: 'Georgia', value: 'GA', latitude: 32.1656, longitude: -82.9001 },
-  ];
+    { label: 'Lahore', value: 'LHR', latitude: 31.5497, longitude: 74.3436 },
+    { label: 'Islamabad', value: 'ISB', latitude: 33.6844, longitude: 73.0479 },
+  ];  
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const [value, setValue] = useState('NY');
   const [long,setLong] = useState('-74.0060');
   const [lat,setLat] = useState('40.7128');
   const [isFocus, setIsFocus] = useState(false);
-  const [temperatureUnit, setTemperatureUnit] = useState('celsius');
+  const [temperatureUnit, setTemperatureUnit] = useState('metric');
+
+  const [currtemp, setcurrTemp] = useState('0');
+  const [highTemperature, setHighTemperature] = useState('0');
+  const [lowTemperature, setLowTemperature] = useState('0');
+  const [weatherstate, setweatherState] = useState('Sunny');
+  const [weatherPredictions, setWeatherPredictions] = useState([]);
+  const [nextday, setnextdays]= useState([]);
 
   const apiKey = '322d5e6f8be8ff587e21d0cac53d4f0c';
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=${temperatureUnit}&appid=${apiKey}`;
 
   
   const showTemperatureOptions = () => {
@@ -35,13 +45,13 @@ export default function App() {
         {
           text: 'Celsius',
           onPress: () => {
-            setTemperatureUnit('celsius');
+            setTemperatureUnit('metric');
           }
         },
         {
           text: 'Fahrenheit',
           onPress: () => {
-            setTemperatureUnit('fahrenheit');
+            setTemperatureUnit('imperial');
           }
         },
         {
@@ -72,15 +82,60 @@ export default function App() {
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        console.log('API Response Data:', data);
-      })
+        setcurrTemp(Math.ceil(data.list[0].main.temp));
+        setHighTemperature(Math.ceil(data.list[0].main.temp_max));
+        setLowTemperature(Math.floor(data.list[0].main.temp_min));
+        setweatherState(data.list[0].weather[0].main);    
+
+        setWeatherPredictions(data.list.slice(1, 4))
+
+        const currentDate = new Date();
+        const nextdays = new Date(currentDate);
+        const nextDaysName =[]
+        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        for(var i=0;i<3;i++){
+          nextdays.setDate(currentDate.getDate() + i+1);
+          const nextDaysOfWeek = nextdays.getDay();
+          nextDaysName.push(daysOfWeek[nextDaysOfWeek]);
+        }
+        setnextdays(nextDaysName);
+        setIsLoading(false);
+       })
       .catch(error => {
         console.log('Error!', error);
+        setIsLoading(false);
       });
-  },[lat,long])
+    
+  },[lat,long,temperatureUnit])
+
+  const getBackgroundColor = () => {
+    if (weatherstate === 'Clear') {
+      return '#48AEFF'; 
+    } else if (weatherstate === 'Rain') {
+      return '#3F7CD7';
+    } else if (weatherstate === 'Clouds') {
+      return '#37B8FC';
+    } else if (weatherstate === 'Snow') {
+      return '#37B8FC';
+    } else if (weatherstate === 'Drizzle') {
+      return '#3F7CD7';
+    } else if (weatherstate === 'Thunderstorm') {
+      return '#6840A3'; 
+    } else {
+      return 'blue';
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container,{backgroundColor:getBackgroundColor()}]}>
       <View style={styles.header}>
       <View style={styles.header1}>
       <View>
@@ -110,20 +165,31 @@ export default function App() {
 
 
       <View style={styles.icon}>
-      <Ionicons name="sunny-sharp" size={250} color="#FFCC33" />
+        {weatherstate === 'Clear' ? (
+          <Ionicons name="sunny-sharp" size={250} color="#FFCC33" />
+        ) : weatherstate === 'Rain' ? (
+          <Ionicons name="rainy" size={250} color="#E8E8EA" />
+        ) : weatherstate === 'Clouds' ? (
+          <FontAwesome name="cloud" size={250} color="#E8E8EA" />
+        ) : weatherstate === 'Snow' ? (
+          <Ionicons name="snow" size={250} color="#FFFFFF" />
+        ) : weatherstate === 'Drizzle' ? (
+          <Feather name="cloud-drizzle" size={250} color="#FFFFFF" /> 
+        ) : weatherstate === 'Thunderstorm' ? (
+          <Ionicons name="thunderstorm-sharp" size={250} color="#413554" />
+        ) : null}
       </View>
-
 
 
       <View style={styles.body}>
       <View style={styles.body1}>
-      <Text style={{ fontSize: 75,fontWeight:'bold'}}>22</Text>
+      <Text style={{ fontSize: 75,fontWeight:'bold'}}>{currtemp}</Text>
       </View>
       <View style={styles.body2}>
-        <Text style={{fontSize:30}}>SUNNY</Text>
+        <Text style={{fontSize:30}}>{weatherstate}</Text>
       </View>
       <View style={styles.body3}>
-        <Text style={{fontSize:30}}>22/14</Text>
+        <Text style={{fontSize:30}}>{highTemperature}/{lowTemperature}</Text>
       </View>
       </View>
 
@@ -132,37 +198,89 @@ export default function App() {
       <View style={styles.bottom}>
         <View style={styles.bottomcol}>
           <View style={styles.bottomcol1}>
-          <Feather name="sun" size={50} color="black" />
+          {(() => {
+            const weatherState = weatherPredictions[0].weather[0].main;
+            if (weatherState === 'Clear') {
+              return <Ionicons name="sunny-sharp" size={50} color="#FFCC33" />;
+            } else if (weatherState === 'Rain') {
+              return <Ionicons name="rainy" size={50} color="#E8E8EA" />;
+            } else if (weatherState === 'Clouds') {
+              return <FontAwesome name="cloud" size={50} color="#E8E8EA" />;
+            } else if (weatherState === 'Snow') {
+              return <Ionicons name="snow" size={50} color="#FFFFFF" />;
+            } else if (weatherState === 'Drizzle') {
+              return <Feather name="cloud-drizzle" size={50} color="#FFFFFF" />;
+            } else if (weatherState === 'Thunderstorm') {
+              return <Ionicons name="thunderstorm-sharp" size={50} color="#413554" />;
+            } else {
+              return null;
+            }
+          })()}
           </View>
         <View style={styles.bottomcol2}>
-          <Text style={{fontSize:30}}>Today</Text>
+          <Text style={{fontSize:25}}>{nextday[0]}</Text>
         </View>
         <View style={styles.bottomcol3}>
-          <Text style={{fontSize:24}}>22/14</Text>
+          <Text style={{fontSize:24}}>{Math.ceil(weatherPredictions[0].main.temp_max)}/{Math.floor(weatherPredictions[0].main.temp_min)}</Text>
         </View>
         </View>
 
         <View style={styles.bottomcol}>
         <View style={styles.bottomcol1}>
-        <Ionicons name="cloud-outline" size={50} color="black" />
+        {(() => {
+            const weatherState = weatherPredictions[1].weather[0].main;
+            if (weatherState === 'Clear') {
+              return <Ionicons name="sunny-sharp" size={50} color="#FFCC33" />;
+            } else if (weatherState === 'Rain') {
+              return <Ionicons name="rainy" size={50} color="#E8E8EA" />;
+            } else if (weatherState === 'Clouds') {
+              return <FontAwesome name="cloud" size={50} color="#E8E8EA" />;
+            } else if (weatherState === 'Snow') {
+              return <Ionicons name="snow" size={50} color="#FFFFFF" />;
+            } else if (weatherState === 'Drizzle') {
+              return <Feather name="cloud-drizzle" size={50} color="#FFFFFF" />;
+            } else if (weatherState === 'Thunderstorm') {
+              return <Ionicons name="thunderstorm-sharp" size={50} color="#413554" />;
+            } else {
+              return null;
+            }
+          })()}
           </View>
         <View style={styles.bottomcol2}>
-          <Text style={{fontSize:30}}>Monday</Text>
+          <Text style={{fontSize:25}}>{nextday[1]}</Text>
         </View>
         <View style={styles.bottomcol3}>
-          <Text style={{fontSize:24}}>20/12</Text>
+          <Text style={{fontSize:24}}>{Math.ceil(weatherPredictions[1].main.temp_max)}/{Math.floor(weatherPredictions[1].main.temp_min)}</Text>
         </View>
         </View>
 
         <View style={styles.bottomcol}>
         <View style={styles.bottomcol1}>
-        <Feather name="cloud-drizzle" size={50} color="black" />
+        {(() => {
+            const weatherState = weatherPredictions[2].weather[0].main;
+            if (weatherState === 'Clear') {
+              return <Ionicons name="sunny-sharp" size={50} color="#FFCC33" />;
+            } else if (weatherState === 'Rain') {
+              return <Ionicons name="rainy" size={50} color="#E8E8EA" />;
+            } else if (weatherState === 'Clouds') {
+              return <FontAwesome name="cloud" size={50} color="#E8E8EA" />;
+            } else if (weatherState === 'Snow') {
+              return <Ionicons name="snow" size={50} color="#FFFFFF" />;
+            } else if (weatherState === 'Drizzle') {
+              return <Feather name="cloud-drizzle" size={50} color="#FFFFFF" />;
+            } else if (weatherState === 'Thunderstorm') {
+              return <Ionicons name="thunderstorm-sharp" size={50} color="#413554" />;
+            } else {
+              return null;
+            }
+          })()}
           </View>
         <View style={styles.bottomcol2}>
-          <Text style={{fontSize:30}}>Thursday</Text>
+          <Text style={{fontSize:25}}>{nextday[2]}</Text>
         </View>
         <View style={styles.bottomcol3}>
-          <Text style={{fontSize:24}}>19/10</Text>
+      
+          <Text style={{fontSize:24}}>{Math.ceil(weatherPredictions[2].main.temp_max)}/{Math.floor(weatherPredictions[2].main.temp_min)}</Text>
         </View>
         </View>
 
@@ -170,14 +288,13 @@ export default function App() {
       </View>
 
     </View>
-  );
+  );  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#49CBFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
